85a9dbfc9524b67f83553fdbd229196d4e7bdc99
jimmy-collazos <jimmy.collazos@gmail.com>
2021-05-19 23:24:02 +0200

    add test for calc/mul module

---
+++ src/calc/mul.rs
@@ -0,0 +1,22 @@
+pub fn mul(left:i32, right:i32) -> i32 {
+    return 0;
+}
+
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn cero() {
+        assert_eq!(0, mul(0, 0));
+    }
+
+    #[test]
+    fn multiplication() {
+        assert_eq!(1, mul(1, 1));
+    }
+
+    #[test]
+    fn negative_multiplication(){
+        assert_eq!(-100, mul(-10, 10))
+    }  
+}

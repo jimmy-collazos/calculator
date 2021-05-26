c07e9ed203e78fb8800646eac912b7ce6ced4c5f
jimmy-collazos <jimmy.collazos@gmail.com>
2021-05-26 14:22:21 +0200

    refactor: implement bakhshali approximation for sqrt()

--- src/calc/sqrt.rs
+++ src/calc/sqrt.rs
@@ -1,8 +1,44 @@
+// @see https://www.geeksforgeeks.org/bakhshali-approximation-computing-square-roots/
 pub fn sqrt(x:i32) -> i32 {
+
     if x < 0 {
         panic!("Guess value must be positive number: {}", x);
     }
-    return (x as f64).sqrt() as i32;
+
+    // This will be the nearest perfect square to s
+    let mut nearest_perfect_square = 0;
+     
+    // This is the sqrt of nearest_perfect_square
+    let mut sqrt_near_perfect_square = 0;
+ 
+    // Find the nearest perfect square to s
+    let mut i = x;
+    while i > 0 {
+        for j in 1..i {
+            if j*j == i {
+                nearest_perfect_square = i;
+                sqrt_near_perfect_square = j;
+                break;
+            }
+        }
+        if nearest_perfect_square > 0 {
+            break;
+        }
+        i -= 1;
+    }
+     
+    // calculate d   
+    let d = x - nearest_perfect_square;    
+     
+    // calculate P
+    let p = d / 2 * sqrt_near_perfect_square;
+     
+    // calculate A
+    let a = sqrt_near_perfect_square + p;
+      
+    // calculate sqrt(S).
+    let sqrt_of_x = a - (p * p / 2 * a);
+    return sqrt_of_x;
 }
 
 pub mod test {
@@ -16,6 +52,8 @@ pub mod test {
     #[test]
     fn squareroot() {
         assert_eq!(3, sqrt(9));
+        assert_eq!(5, sqrt(25));
+        assert_eq!(75, sqrt(75*75));
     }
 
     #[test]

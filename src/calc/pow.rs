0d1c8c8a70ead7c6a50297d1ebdd15122f3fda6d
jimmy-collazos <jimmy.collazos@gmail.com>
2022-06-04 17:58:08 +0200

    feat: add unit tests for pow function

--- src/calc/pow.rs
+++ src/calc/pow.rs
@@ -1,3 +1,43 @@
-pub fn pow(base:i32, exponent:i32) -> i32 {
+pub fn pow(mut base:i32, mut exponent:i32) -> i32 {
   return 0;
+}
+
+pub mod pow_test {
+  use super::*;
+
+  #[test]
+  fn cero_exponent() {
+    let base = 10;
+    let exponent = 0;
+    let result = pow(base, exponent);
+    let expect = 1;
+    assert_eq!(expect, result);
+  }
+
+  #[test]
+  fn one_exponent() {
+    let base = 10;
+    let exponent = 1;
+    let result = pow(base, exponent);
+    let expect = base;  
+    assert_eq!(expect, result);
+  }
+
+  #[test]
+  fn square_exponent() {
+    let base = 2;
+    let exponent = 2;
+    let result = pow(base, exponent);
+    let expect = base * base;  
+    assert_eq!(expect, result);
+  }
+
+  #[test]
+  fn cubic_exponent() {
+    let base = 2;
+    let exponent = 3;
+    let result = pow(base, exponent);
+    let expect = base * base * base;
+    assert_eq!(expect, result);
+  }
 }
